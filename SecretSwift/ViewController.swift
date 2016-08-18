19e8b05cc0ca14047f694583153d3a8eef80afec
My Nguyen <My@mys-macbook-pro.local>
2016-08-17 19:24:54 -0700

    The basic text editor

--- SecretSwift/ViewController.swift
+++ SecretSwift/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var secret: UITextView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let notificationCenter = NSNotificationCenter.defaultCenter()
+        // receive notification when the keyboard hides
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIKeyboardWillHideNotification, object: nil)
+        // receive notification when the keyboard changes
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIKeyboardWillChangeFrameNotification, object: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func authenticateUser(sender: AnyObject) {
+    }
 
+    // see project Javascript-Safari for a detailed explanation of this method
+    func adjustForKeyboard(notification: NSNotification) {
+        let userInfo = notification.userInfo!
+
+        let keyboardScreenEndFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).CGRectValue()
+        let keyboardViewEndFrame = view.convertRect(keyboardScreenEndFrame, fromView: view.window)
+
+        if notification.name == UIKeyboardWillHideNotification {
+            secret.contentInset = UIEdgeInsetsZero
+        } else {
+            secret.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height, right: 0)
+        }
+
+        secret.scrollIndicatorInsets = secret.contentInset
+
+        let selectedRange = secret.selectedRange
+        secret.scrollRangeToVisible(selectedRange)
+    }
 }
 
